6edb8073e113da939f9b0197b6eaeed5e7b8c8b2
Burak <burak_kurtarir@epam.com>
2023-08-24 14:42:26 +0300

    created character model

---
+++ lib/pages/characters/model/character_model.dart
@@ -0,0 +1,39 @@
+class CharacterModel {
+  final int? id;
+  final String? name;
+  final String? status;
+  final String? species;
+  final String? type;
+  final String? gender;
+  final Location? origin;
+  final Location? location;
+  final String? image;
+  final List<String>? episode;
+  final String? url;
+  final DateTime? created;
+
+  const CharacterModel({
+    this.id,
+    this.name,
+    this.status,
+    this.species,
+    this.type,
+    this.gender,
+    this.origin,
+    this.location,
+    this.image,
+    this.episode,
+    this.url,
+    this.created,
+  });
+}
+
+class Location {
+  final String? name;
+  final String? url;
+
+  const Location({
+    this.name,
+    this.url,
+  });
+}
