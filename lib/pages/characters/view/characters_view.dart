43a42460121596419d37c556671de5e5cc69819d
Burak <burak_kurtarir@epam.com>
2023-08-24 14:38:35 +0300

    added theme and added characters view

---
+++ lib/pages/characters/view/characters_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CharactersView extends StatelessWidget {
+  const CharactersView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Characters'),
+      ),
+      body: Container(),
+    );
+  }
+}
